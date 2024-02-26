2c7e96c5a804466e432565dc1d5c15c7ec419e05
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2024-02-26 19:08:28 -0300

    header component | global styles | _variables.scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
